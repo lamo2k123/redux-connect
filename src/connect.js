65d92e7bf0ca19eaaf13755b94bf2a1614c4f2c8
lamo2k123 <lamo2k123@gmail.com>
2016-05-20 17:12:03 +0300

    init

---
+++ src/connect.js
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react'
+
+const store = PropTypes.shape({
+    dispatch    : PropTypes.func.isRequired,
+    getState    : PropTypes.func.isRequired,
+    subscribe   : PropTypes.func.isRequired
+}).isRequired;
+
+export default ComponentConnect => class extends Component {
+
+    static contextTypes = { store };
+
+    static propTypes = { store }
+
+    constructor() {
+        super(...arguments);
+
+        this.state = this.connect(this.store.getState());
+    }
+
+    get store() {
+        return this.props.store || this.context.store;
+    }
+
+    get connect() {
+        let connect = state => ({});
+
+        if(typeof ComponentConnect.connect == 'function') {
+            connect = ComponentConnect.connect;
+        }
+
+        return connect;
+    }
+
+    subscribe = () => {
+        let state = JSON.stringify(this.state),
+            store = JSON.stringify(this.connect(this.store.getState()));
+
+        if(state != store) {
+            this.setState(this.connect(this.store.getState()));
+        }
+    }
+
+    componentDidMount() {
+        this.unsubscribe = this.store.subscribe(this.subscribe);
+    }
+
+    componentWillUnmount() {
+        if(this.unsubscribe) {
+            this.unsubscribe();
+
+            this.unsubscribe = null;
+        }
+    }
+
+    render() {
+        return <ComponentConnect {...this.props} {...this.state} />
+    }
+
+}
---
+++ src/provider.js
@@ -0,0 +1,34 @@
+import { Component, PropTypes, Children } from 'react'
+
+const store = PropTypes.shape({
+    dispatch    : PropTypes.func.isRequired,
+    getState    : PropTypes.func.isRequired,
+    subscribe   : PropTypes.func.isRequired
+}).isRequired;
+
+export default class extends Component {
+
+    static propTypes = {
+        children: PropTypes.element.isRequired,
+        store
+    }
+
+    static childContextTypes = {
+        store
+    }
+
+    getChildContext() {
+        return { store: this._store }
+    }
+
+    constructor(props) {
+        super(...arguments);
+
+        console.log('PROVIDER props', props);
+        this._store = props.store
+    }
+
+    render() {
+        return Children.only(this.props.children)
+    }
+}
